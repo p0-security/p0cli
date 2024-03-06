a5456875db483504f84e501109ba03bd2905d2a7
komal-dhull <117865035+komal-dhull@users.noreply.github.com>
2024-03-06 13:36:55 -0800

    Prepare for publishing CLI publicly: rename and bump minor version (#53)
    Rename @p0security/p0cli to @p0security/cli. Also bump version to 0.3.0
    prior to publishing.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 /** Copyright © 2024-present P0 Security
 
-This file is part of @p0security/p0cli
+This file is part of @p0security/cli
 
-@p0security/p0cli is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License.
+@p0security/cli is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License.
 
-@p0security/p0cli is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+@p0security/cli is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with @p0security/p0cli. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with @p0security/cli. If not, see <https://www.gnu.org/licenses/>.
 **/
 import { cli } from "./commands";
 import { noop } from "lodash";
