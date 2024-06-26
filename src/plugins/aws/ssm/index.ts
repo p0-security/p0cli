c65dc505c83cae5ff389f9fa9255f8081fef88b5
Gergely Danyi <gergely.danyi@p0.dev>
2024-06-26 13:15:20 -0500

    Remove unused code

--- src/plugins/aws/ssm/index.ts
+++ src/plugins/aws/ssm/index.ts
@@ -25,13 +25,9 @@ import {
   ChildProcessByStdio,
   StdioNull,
   StdioPipe,
-  exec,
   spawn,
 } from "node:child_process";
-import { Readable, Transform, Writable } from "node:stream";
-import psTree from "ps-tree";
-
-const STARTING_SESSION_MESSAGE = /Starting session with SessionId: (.*)/;
+import { Readable, Writable } from "node:stream";
 
 /** Matches the error message that AWS SSM print1 when access is not propagated */
 // Note that the resource will randomly be either the SSM document or the EC2 instance
@@ -58,7 +54,6 @@ const UNPROVISIONED_ACCESS_VALIDATION_WINDOW_MS = 5e3;
 const MAX_SSM_RETRIES = 30;
 
 /** The name of the SessionManager port forwarding document. This document is managed by AWS.  */
-const LOCAL_PORT_FORWARDING_DOCUMENT_NAME = "AWS-StartPortForwardingSession";
 const START_SSH_SESSION_DOCUMENT_NAME = "AWS-StartSSHSession";
 
 type SsmArgs = {
@@ -124,66 +119,6 @@ const createBaseSsmCommand = (args: Pick<SsmArgs, "instance" | "region">) => {
     "--target",
     args.instance,
   ];
-};
-
-const createInteractiveShellCommand = (
-  args: Omit<SsmArgs, "forwardPortAddress" | "requestId">
-) => {
-  const ssmCommand = [
-    ...createBaseSsmCommand(args),
-    "--document-name",
-    args.documentName,
-  ];
-
-  const command = args.command?.trim();
-  if (command) {
-    ssmCommand.push("--parameters", `command='${command}'`);
-  }
-
-  return ssmCommand;
-};
-
-const createPortForwardingCommand = (
-  args: Omit<SsmArgs, "requestId"> &
-    Required<Pick<SsmArgs, "forwardPortAddress">>
-) => {
-  const [localPort, remotePort] = args.forwardPortAddress
-    .split(":")
-    .map(Number);
-
-  return [
-    ...createBaseSsmCommand(args),
-    "--document-name",
-    // Port forwarding is a special case that uses an AWS-managed document, not the user-generated document we use for an SSH session
-    LOCAL_PORT_FORWARDING_DOCUMENT_NAME,
-    "--parameters",
-    `localPortNumber=${localPort},portNumber=${remotePort}`,
-  ];
-};
-
-type SsmCommands = { shellCommand: string[]; subCommand?: string[] };
-
-const createSsmCommands = (args: Omit<SsmArgs, "requestId">): SsmCommands => {
-  const interactiveShellCommand = createInteractiveShellCommand(args);
-
-  const forwardPortAddress = args.forwardPortAddress;
-  if (!forwardPortAddress) {
-    return { shellCommand: interactiveShellCommand };
-  }
-
-  const portForwardingCommand = createPortForwardingCommand({
-    ...args,
-    forwardPortAddress,
-  });
-
-  if (args.noRemoteCommands) {
-    return { shellCommand: portForwardingCommand };
-  }
-
-  return {
-    shellCommand: interactiveShellCommand,
-    subCommand: portForwardingCommand,
-  };
 };
 
 function spawnChildProcess(
@@ -294,170 +229,6 @@ async function spawnSsmNode(
   });
 }
 
-/**
- * A subprocess SSM session redirects its output through a proxy that filters certain messages reducing the verbosity of the output.
- * The subprocess also makes sure to terminate any grandchild processes that might spawn during the session.
- *
- * This process should be used when multiple SSM sessions need to be spawned in parallel.
- */
-const spawnSubprocessSsmNode = async (options: {
-  credential: AwsCredentials;
-  command: string[];
-  attemptsRemaining?: number;
-  abortController: AbortController;
-}): Promise<number | null> =>
-  new Promise((resolve, reject) => {
-    const child = spawnChildProcess(
-      options.credential,
-      "/usr/bin/env",
-      options.command,
-      ["ignore", "pipe", "pipe"]
-    );
-
-    // Captures the starting session message and filters it from the output
-    const proxyStream = new Transform({
-      transform(chunk, _, end) {
-        const message = chunk.toString("utf-8");
-        const match = message.match(STARTING_SESSION_MESSAGE);
-        if (!match) {
-          this.push(chunk);
-        }
-        end();
-      },
-    });
-
-    // Ensures that content from the child process is printed to the terminal and the proxy stream
-    child.stdout.pipe(proxyStream).pipe(process.stdout);
-
-    const { isAccessPropagated } = accessPropagationGuard(child);
-
-    const abortListener = (code: any) => {
-      options.abortController.signal.removeEventListener(
-        "abort",
-        abortListener
-      );
-
-      // AWS CLI typically will spawn a grandchild process for the SSM session. Using `ps-tree` will allow us
-      // to identify and terminate the grandchild process as well.
-      psTree(child.pid!, function (_, children) {
-        // kill the original child process first so that messages from grandchildren are not printed to stdout
-        child.kill();
-        // Send a SIGTERM because other signals (e.g. SIGKILL) will not propagate to the grandchildren
-        exec(`kill -15 ${children.map((p) => p.PID).join(" ")}`);
-      });
-
-      resolve(code);
-    };
-
-    child.on("spawn", () => {
-      options.abortController.signal.addEventListener("abort", abortListener);
-    });
-
-    const exitListener = child.on("exit", (code) => {
-      exitListener.unref();
-
-      // In the case of ephemeral AccessDenied exceptions due to unpropagated
-      // permissions, continually retry access until success
-      if (!isAccessPropagated()) {
-        options.abortController.signal.removeEventListener(
-          "abort",
-          abortListener
-        );
-
-        const attemptsRemaining = options?.attemptsRemaining ?? MAX_SSM_RETRIES;
-        if (attemptsRemaining <= 0) {
-          reject(
-            "Access did not propagate through AWS before max retry attempts were exceeded. Please contact support@p0.dev for assistance."
-          );
-          return;
-        }
-
-        spawnSubprocessSsmNode({
-          ...options,
-          attemptsRemaining: attemptsRemaining - 1,
-        })
-          .then((code) => resolve(code))
-          .catch(reject);
-        return;
-      }
-
-      options.abortController.abort(code);
-    });
-  });
-
-/** Convert an SshCommandArgs into an SSM document "command" parameter */
-const commandParameter = (args: SshCommandArgs) =>
-  args.command
-    ? `${args.command} ${args.arguments
-        .map(
-          (argument) =>
-            // escape all double quotes (") in commands such as `p0 ssh <instance>> echo 'hello; "world"'` because we
-            // need to encapsulate command arguments in double quotes as we pass them along to the remote shell
-            `"${String(argument).replace(/"/g, '\\"')}"`
-        )
-        .join(" ")}`.trim()
-    : undefined;
-
-/** Connect to an SSH backend using AWS Systems Manager (SSM) */
-export const ssm = async (
-  authn: Authn,
-  request: ExerciseGrantResponse,
-  args: SshCommandArgs
-) => {
-  const isInstalled = await ensureSsmInstall();
-  if (!isInstalled)
-    throw "Please try again after installing the required AWS utilities";
-  const credential = await assumeRoleWithOktaSaml(authn, {
-    account: request.instance.accountId,
-    role: request.role,
-  });
-  const ssmArgs = {
-    instance: request.instance.id,
-    region: request.instance.region,
-    documentName: request.documentName,
-    forwardPortAddress: args.L,
-    noRemoteCommands: args.N,
-    command: commandParameter(args),
-  };
-
-  const ssmCommands = createSsmCommands(ssmArgs);
-
-  await startSsmProcesses(credential, ssmCommands);
-};
-
-/**
- * Starts the SSM session and any additional processes that are requested for the session to function properly.
- */
-const startSsmProcesses = async (
-  credential: AwsCredentials,
-  commands: SsmCommands
-) => {
-  /** The AbortController is responsible for sending a shared signal to all spawned processes ({@link spawnSsmNode}) when the parent process is terminated unexpectedly. This is necessary because the spawned processes are detached and would otherwise continue running after the parent process is terminated. */
-  const abortController = new AbortController();
-
-  const args = { credential, abortController };
-  const processes: Promise<unknown>[] = [
-    spawnSsmNode({
-      ...args,
-      command: "/usr/bin/env",
-      args: commands.shellCommand,
-      stdio: ["inherit", "inherit", "pipe"],
-      sshAgentEnv: {} as SshAgentEnv, // TODO remove this once port-forwarding and interactive commands also use the ssh-agent
-    }),
-  ];
-
-  if (commands.subCommand) {
-    processes.push(
-      spawnSubprocessSsmNode({
-        ...args,
-        command: commands.subCommand,
-      })
-    );
-  }
-
-  await Promise.all(processes);
-};
-
 const createProxyCommands = (
   data: ExerciseGrantResponse,
   args: ScpCommandArgs | SshCommandArgs,
@@ -511,7 +282,12 @@ const createProxyCommands = (
       ...(args.N ? ["-N"] : []),
       `${data.linuxUserName}@${data.instance.id}`,
       ...(args.command ? [args.command] : []),
-      ...args.arguments,
+      ...args.arguments.map(
+        (argument) =>
+          // escape all double quotes (") in commands such as `p0 ssh <instance>> echo 'hello; "world"'` because we
+          // need to encapsulate command arguments in double quotes as we pass them along to the remote shell
+          `"${String(argument).replace(/"/g, '\\"')}"`
+      ),
     ],
   };
 };
