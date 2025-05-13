6ee5c01e6e5029a301d511a94317e6394784d66b
fabgo <fabsphoto@outlook.com>
2025-05-13 14:58:57 -0700

    ENG-4447 Enhance `p0 login` to be a no-op if user is currently logged in (#188)
    * Do not log in again if user is currently logged in
    * Output the currently logged-in P0 organization
    * Output the remaining session time
    * Add --refresh optional argument to force re-authentication
    * Added unit tests to cover new login scenarios

--- src/commands/__tests__/login.test.ts
+++ src/commands/__tests__/login.test.ts
@@ -8,9 +8,13 @@ This file is part of @p0security/cli
 
 You should have received a copy of the GNU General Public License along with @p0security/cli. If not, see <https://www.gnu.org/licenses/>.
 **/
+import * as auth from "../../drivers/auth";
+import * as config from "../../drivers/config";
 import { bootstrapConfig } from "../../drivers/env";
+import { print2 } from "../../drivers/stdio";
 import { pluginLoginMap } from "../../plugins/login";
 import { mockGetDoc } from "../../testing/firestore";
+import { Identity } from "../../types/identity";
 import { login } from "../login";
 import { signInWithCredential } from "firebase/auth";
 import { readFile, writeFile } from "fs/promises";
@@ -20,19 +24,37 @@ jest.mock("fs/promises");
 jest.mock("../../drivers/auth/path", () => ({
   getIdentityFilePath: jest.fn(() => "/dummy/identity/file/path"),
 }));
-jest.mock("../../drivers/config", () => ({
-  ...jest.requireActual("../../drivers/config"),
-  saveConfig: jest.fn(),
-  loadConfig: jest.fn(() => bootstrapConfig),
-}));
 jest.mock("../../drivers/stdio");
 jest.mock("../../plugins/login");
+
+const mockIdentity: Identity = {
+  credential: {
+    expires_at: Date.now() * 1e-3 + 60 * 1000,
+  },
+  org: {
+    tenantId: "test-tenant",
+    slug: "test-org",
+  },
+} as Identity;
 
 const mockSignInWithCredential = signInWithCredential as jest.Mock;
 const mockReadFile = readFile as jest.Mock;
 const mockWriteFile = writeFile as jest.Mock;
 
 describe("login", () => {
+  beforeEach(() => {
+    jest.spyOn(config, "loadConfig").mockResolvedValueOnce(bootstrapConfig);
+    jest.spyOn(config, "saveConfig").mockImplementation(jest.fn());
+    // do NOT spyOn getContactMessage — you want the real one
+  });
+
+  it("prints a friendly error if the org is not provided", async () => {
+    mockGetDoc(undefined);
+    await expect(login({})).rejects.toMatchInlineSnapshot(
+      `"The P0 organization ID is required. Please provide it as an argument or set the P0_ORG environment variable."`
+    );
+  });
+
   it("prints a friendly error if the org is not found", async () => {
     mockGetDoc(undefined);
     await expect(login({ org: "test-org" })).rejects.toMatchInlineSnapshot(
@@ -40,7 +62,7 @@ describe("login", () => {
     );
   });
 
-  it("should print a friendly error if unsupported login", async () => {
+  it("prints a friendly error if unsupported login", async () => {
     mockGetDoc({
       slug: "test-org",
       tenantId: "test-tenant",
@@ -49,6 +71,31 @@ describe("login", () => {
     await expect(login({ org: "test-org" })).rejects.toMatchInlineSnapshot(
       `"Unsupported login for your organization"`
     );
+  });
+
+  describe("identity file does not exist", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+
+      // Mock `readFile` to throw an "ENOENT" error
+      mockReadFile.mockImplementation(() => {
+        const error = new Error("File not found");
+        (error as any).code = "ENOENT";
+        return Promise.reject(error);
+      });
+
+      mockGetDoc({
+        slug: "test-org",
+        tenantId: "test-tenant",
+        ssoProvider: "google",
+      });
+    });
+
+    it("it should ask user to log in", async () => {
+      await expect(login({ org: "test-org" })).rejects.toMatchInlineSnapshot(
+        `"Please run \`p0 login <organization>\` to use the P0 CLI."`
+      );
+    });
   });
 
   describe("organization exists", () => {
@@ -95,7 +142,7 @@ describe("login", () => {
       expect((signInWithCredential as jest.Mock).mock.calls).toMatchSnapshot();
     });
 
-    it("returns an error message if firebase cannot determine the user's email", async () => {
+    it("returns an error message if firebase cannot determine the user email", async () => {
       mockSignInWithCredential.mockResolvedValueOnce({
         user: {},
       });
@@ -104,30 +151,65 @@ describe("login", () => {
 Please contact support@p0.dev for assistance."
 `);
     });
-  });
 
-  describe("identity file does not exist", () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
+    describe("already logged in", () => {
+      beforeEach(() => {
+        jest.clearAllMocks();
 
-      // Mock `readFile` to throw an "ENOENT" error
-      mockReadFile.mockImplementation(() => {
-        const error = new Error("File not found");
-        (error as any).code = "ENOENT";
-        return Promise.reject(error);
+        jest.spyOn(auth, "loadCredentials").mockResolvedValue(mockIdentity);
+
+        mockGetDoc({
+          slug: "test-org",
+          tenantId: "test-tenant",
+          ssoProvider: "google",
+        });
       });
 
-      mockGetDoc({
-        slug: "test-org",
-        tenantId: "test-tenant",
-        ssoProvider: "google",
-      });
-    });
+      it("no org provided, prints current logged-in status", async () => {
+        await login({});
 
-    it("it should ask user to log in", async () => {
-      await expect(login({ org: "test-org" })).rejects.toMatchInlineSnapshot(
-        `"Please run \`p0 login <organization>\` to use the P0 CLI."`
-      );
+        expect(print2).toHaveBeenCalledWith(
+          "You are currently logged in to the test-org organization."
+        );
+
+        expect(print2).toHaveBeenCalledWith(
+          expect.stringContaining("The current session expires in ")
+        );
+      });
+
+      it("org provided, prints already logged-in status", async () => {
+        await login({ org: "test-org" });
+
+        expect(print2).toHaveBeenCalledWith(
+          "You are already logged in to the test-org organization."
+        );
+
+        expect(print2).toHaveBeenCalledWith(
+          expect.stringContaining("The current session expires in ")
+        );
+      });
+
+      it("--refresh provided, need to re-login", async () => {
+        await login({ org: "test-org", refresh: true });
+
+        expect(print2).toHaveBeenCalledWith(
+          "You are now logged in to the test-org organization, and can use the p0 CLI."
+        );
+      });
+
+      it("different org provided, need to re-login", async () => {
+        mockGetDoc({
+          slug: "other-org",
+          tenantId: "other-tenant",
+          ssoProvider: "google",
+        });
+
+        await login({ org: "other-org" });
+
+        expect(print2).toHaveBeenCalledWith(
+          "You are now logged in to the other-org organization, and can use the p0 CLI."
+        );
+      });
     });
   });
 });
