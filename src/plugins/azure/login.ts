cb5866bd95b702b045a20b3d3208d470f8ab2e94
Miguel Campos <miguel.campos@p0.dev>
2025-08-05 14:39:28 -0700

    fix entra login

--- src/plugins/azure/login.ts
+++ src/plugins/azure/login.ts
@@ -8,25 +8,95 @@ This file is part of @p0security/cli
 
 You should have received a copy of the GNU General Public License along with @p0security/cli. If not, see <https://www.gnu.org/licenses/>.
 **/
-import { TokenResponse } from "../../types/oidc";
+import { OIDC_HEADERS } from "../../common/auth/oidc";
+import { withRedirectServer } from "../../common/auth/server";
+import { urlEncode, validateResponse } from "../../common/fetch";
+import { print2 } from "../../drivers/stdio";
+import { AuthorizeRequest, TokenResponse } from "../../types/oidc";
 import { OrgData } from "../../types/org";
-import { oidcLogin, oidcLoginSteps } from "../oidc/login";
+import open from "open";
+import pkceChallenge from "pkce-challenge";
 
 const AZURE_SCOPE = "openid profile email offline_access";
+const AZURE_REDIRECT_PORT = 52701;
+const AZURE_REDIRECT_URL = `http://localhost:${AZURE_REDIRECT_PORT}`;
+const PKCE_LENGTH = 128;
 
-const azureUrls = (org: OrgData) => {
+type CodeExchange = {
+  code: string;
+  state: string;
+};
+
+const requestAuth = async (org: OrgData) => {
   if (!org.providerDomain) {
     throw "Azure login requires a configured provider domain.";
   }
-  
-  const baseUrl = `https://login.microsoftonline.com/${org.providerDomain}/oauth2/v2.0`;
-  return {
-    deviceAuthorizationUrl: `${baseUrl}/devicecode`,
-    tokenUrl: `${baseUrl}/token`,
+
+  const pkce = await pkceChallenge(PKCE_LENGTH);
+  const baseUrl = `https://login.microsoftonline.com/${org.providerDomain}/oauth2/v2.0/authorize`;
+
+  const authBody: AuthorizeRequest = {
+    client_id: org.clientId,
+    code_challenge: pkce.code_challenge,
+    code_challenge_method: "S256",
+    redirect_uri: AZURE_REDIRECT_URL,
+    response_type: "code",
+    scope: AZURE_SCOPE,
+    state: "azure_login",
   };
+
+  const url = `${baseUrl}?${urlEncode(authBody)}`;
+
+  print2(`Your browser has been opened to visit:
+
+    ${url}`);
+
+  open(url).catch(() => {
+    print2(`Please visit the following URL to continue login:
+
+    ${url}`);
+  });
+
+  return pkce;
+};
+
+const requestToken = async (
+  org: OrgData,
+  code: string,
+  pkce: { code_challenge: string; code_verifier: string }
+) => {
+  if (!org.providerDomain) {
+    throw "Azure login requires a configured provider domain.";
+  }
+
+  const tokenUrl = `https://login.microsoftonline.com/${org.providerDomain}/oauth2/v2.0/token`;
+
+  const body = {
+    client_id: org.clientId,
+    code,
+    code_verifier: pkce.code_verifier,
+    grant_type: "authorization_code",
+    redirect_uri: AZURE_REDIRECT_URL,
+  };
+
+  const response = await fetch(tokenUrl, {
+    method: "POST",
+    headers: {
+      ...OIDC_HEADERS,
+      Accept: "application/json",
+      Origin: AZURE_REDIRECT_URL,
+    },
+    body: urlEncode(body),
+  });
+
+  const valid = await validateResponse(response);
+  return (await valid.json()) as TokenResponse;
 };
 
 export const azureLogin = async (org: OrgData): Promise<TokenResponse> => {
-  const steps = oidcLoginSteps(org, AZURE_SCOPE, () => azureUrls(org));
-  return await oidcLogin<any, TokenResponse>(steps);
+  return await withRedirectServer<any, CodeExchange, TokenResponse>(
+    async () => await requestAuth(org),
+    async (pkce, token) => await requestToken(org, token.code, pkce),
+    { port: AZURE_REDIRECT_PORT }
+  );
 };
