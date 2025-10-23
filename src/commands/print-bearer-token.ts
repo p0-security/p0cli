ec11d8f051f83087145e6bf66452c10e34f1bc54
Gus Boling <gus.boling@p0.dev>
2025-10-23 13:49:28 -0600

    Add print-bearer-token.ts file, containing functionality-stub

---
+++ src/commands/print-bearer-token.ts
@@ -0,0 +1,31 @@
+/** Copyright © 2024-present P0 Security
+
+This file is part of @p0security/cli
+
+@p0security/cli is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License.
+
+@p0security/cli is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with @p0security/cli. If not, see <https://www.gnu.org/licenses/>.
+**/
+
+import { print1 } from "../drivers/stdio";
+import yargs from "yargs";
+
+const printBearerTokenArgs = <T>(yargs: yargs.Argv<T>) => 
+    yargs
+        .help(false)
+        .option("debug", {
+            type: "boolean",
+        describe: "Print debug information.",
+        });
+
+export const printBearerTokenCommand = (yargs: yargs.Argv) =>
+  yargs.command<{
+    debug: boolean;
+}>("print-bearer-token", "Prints bearer-token to stdout", printBearerTokenArgs, printBearerToken);
+
+export const printBearerToken = () => {
+    // TODO: fetch actual bearer token
+    print1("bearer-token-place-holder-value")
+}
