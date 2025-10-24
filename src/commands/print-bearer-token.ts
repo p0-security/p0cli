b389e452df5576befaa3e50d933c70cbaa3a37c4
Gus Boling <gus.boling@p0.dev>
2025-10-24 14:16:52 -0600

    Revise printBearerToken to use getToken method

--- src/commands/print-bearer-token.ts
+++ src/commands/print-bearer-token.ts
@@ -8,10 +8,8 @@ This file is part of @p0security/cli
 
 You should have received a copy of the GNU General Public License along with @p0security/cli. If not, see <https://www.gnu.org/licenses/>.
 **/
+import { authenticate } from "../drivers/auth";
 import { print1 } from "../drivers/stdio";
-import fs from "fs/promises";
-import os from "os";
-import path from "path";
 import yargs from "yargs";
 
 const printBearerTokenArgs = <T>(yargs: yargs.Argv<T>) => yargs.help(false);
@@ -19,22 +17,18 @@ const printBearerTokenArgs = <T>(yargs: yargs.Argv<T>) => yargs.help(false);
 export const printBearerTokenCommand = (yargs: yargs.Argv) =>
   yargs.command(
     "print-bearer-token",
-    "Prints bearer token to stdout",
+    false, // hides command from --help output
     printBearerTokenArgs,
     printBearerToken
   );
 
 export const printBearerToken = async () => {
-  const identityFilePath = path.join(os.homedir(), ".p0", "identity.json");
-  try {
-    const rawData = await fs.readFile(identityFilePath);
-    const identityData = JSON.parse(rawData.toString());
-    print1(identityData.credential?.access_token);
-  } catch (error: any) {
-    if (error?.code == "ENOENT") {
-      throw `Missing identity file.`;
-    } else {
-      throw error;
-    }
+  const authn = await authenticate();
+
+  const token = authn?.identity?.credential?.access_token;
+  if (!token) {
+    console.error("No access token found in identity.");
+    process.exit(1);
   }
+  print1(token);
 };
