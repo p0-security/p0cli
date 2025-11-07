c371401895fd8b80b244ec1dc799cd1b892d8581
Michael Dimitras <michaeladimitras@gmail.com>
2025-11-07 15:58:17 -0800

    Use osSafeCommand for npm and gcloud CLI commands (#304)
    A new function called `osSafeCommand` was introduced in
    https://github.com/p0-security/p0cli/pull/303 for the purpose of running
    shell commands appropriately on both Windows and Mac.
    
    This PR updates two places in the CLI code where this check was already
    being done to use the function instead. There is not functional change
    here. `osSafeCommand` performs the same conditional logic that this code
    was currently doing.'
    
    I tested this by running Gcloud SSH commands on both Windows and Mac. I
    confirmed that the version check and the provisioning of the SSH session
    still work on both.

--- src/plugins/google/util.ts
+++ src/plugins/google/util.ts
@@ -8,22 +8,12 @@ This file is part of @p0security/cli
 
 You should have received a copy of the GNU General Public License along with @p0security/cli. If not, see <https://www.gnu.org/licenses/>.
 **/
-import { getOperatingSystem } from "../../util";
+import { osSafeCommand } from "../../util";
 
 /**
  * Prepends with the operating-system specific method of
  * running a gcloud command.
  * @param args the arguments to be passed to gcloud (excluding "gcloud" itself)
  */
-export const gcloudCommandArgs = (args: string[]) => {
-  const isWindows = getOperatingSystem() === "win";
-
-  // On Windows, when installing the Google Cloud tools, the main gcloud file is
-  // a .cmd (shell script) file rather than a .exe (binary executable) file,
-  // so when calling spawn, it cannot be located except via cmd.exe
-  // Unlike in MacOS, the underlying Windows OS API that spawn uses doesn't
-  // resolve .CMD files by default
-  return isWindows
-    ? { command: "cmd.exe", args: ["/d", "/s", "/c", "gcloud", ...args] }
-    : { command: "gcloud", args };
-};
+export const gcloudCommandArgs = (args: string[]) =>
+  osSafeCommand("gcloud", args);
